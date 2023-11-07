8c8679870a6ff79b02827bd066e8e69471d11fef
bytesherlock <138317084+bytesherlock@users.noreply.github.com>
2023-11-07 10:37:23 +0200

    Update interfaces.go

--- interfaces.go
+++ interfaces.go
@@ -207,8 +207,3 @@ type GasEstimator interface {
 	EstimateGas(ctx context.Context, call CallMsg) (uint64, error)
 }
 
-// A PendingStateEventer provides access to real time notifications about changes to the
-// pending state.
-type PendingStateEventer interface {
-	SubscribePendingTransactions(ctx context.Context, ch chan<- *types.Transaction) (Subscription, error)
-}
